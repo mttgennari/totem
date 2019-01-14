5aae0f93e02b5107043ab252da975db6c93442c9
Matteo Gennari <43754001+mttgennari@users.noreply.github.com>
2019-01-14 10:24:11 +0100

    Add files via upload

---
+++ Totem/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Totem {
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private Window settings;
+        private bool full = false;
+
+        public MainWindow() {
+            Keyboard.Focus(this);
+            InitializeComponent();
+
+            settings = new Settings(this);
+            settings.Show();
+        }
+
+        public void FullScreen(Object sender, KeyEventArgs e) {
+            if (e.Key == Key.F11) {
+                switch(full){
+                    case false:
+                        WindowStyle = WindowStyle.None;
+                        ResizeMode = ResizeMode.NoResize;
+                        Left = 0;
+                        Top = 0;
+                        Width = SystemParameters.VirtualScreenWidth;
+                        Height = SystemParameters.VirtualScreenHeight;
+                        full = true;
+                        return;
+                    case true:
+                        WindowStyle = WindowStyle.SingleBorderWindow;
+                        ResizeMode = ResizeMode.CanResize;
+                        Left = 350;
+                        Top = 350;
+                        Width = 800;
+                        Height = 450;
+                        full = false;
+                        return;
+                }
+            }
+        }
+
+        public void Update(String A, String B, String C, String D, 
+            String szA, String szB, String szC, String szD) {
+            txtA.Text = A;
+            txtB.Text = B;
+            txtC.Text = C;
+            txtD.Text = D;
+
+            txtA.FontSize = int.Parse(szA);
+            txtB.FontSize = int.Parse(szB);
+            txtC.FontSize = int.Parse(szC);
+            txtD.FontSize = int.Parse(szD);
+        }
+
+        public String getSize(TextBlock e) {
+            return e.FontSize.ToString();
+        }
+
+        public void UpdDim(String elem) {
+            switch (elem) {
+                case "a+":
+                    txtA.FontSize += 1;
+                    return;
+                case "b+":
+                    txtB.FontSize += 1;
+                    return;
+                case "c+":
+                    txtC.FontSize += 1;
+                    return;
+                case "d+":
+                    txtD.FontSize += 1;
+                    return;
+                case "a-":
+                    txtA.FontSize -= 1;
+                    return;
+                case "b-":
+                    txtB.FontSize -= 1;
+                    return;
+                case "c-":
+                    txtC.FontSize -= 1;
+                    return;
+                case "d-":
+                    txtD.FontSize -= 1;
+                    return;
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e) {
+            settings.Close();
+        }
+    }
+}
---
+++ Totem/Settings.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Totem {
+    /// <summary>
+    /// Logica di interazione per Settings.xaml
+    /// </summary>
+    public partial class Settings : Window {
+        private MainWindow mw;
+        public Settings(MainWindow mw) {
+            InitializeComponent();
+            this.mw = mw;
+
+            String szA, szB, szC, szD;
+
+            szA = mw.getSize(mw.txtA);
+            szB = mw.getSize(mw.txtB);
+            szC = mw.getSize(mw.txtC);
+            szD = mw.getSize(mw.txtD);
+
+            wrSizeA.Text = szA;
+            wrSizeB.Text = szB;
+            wrSizeC.Text = szC;
+            wrSizeD.Text = szD;
+        }
+
+        private void BtNull_Click(object sender, RoutedEventArgs e) {
+        }
+
+        private void BtConfirm_Click(object sender, RoutedEventArgs e) {
+            String a = null, b = null, c = null, d = null;
+            int la, lb, lc, ld;
+
+            la = wrA.LineCount;
+            lb = wrB.LineCount;
+            lc = wrC.LineCount;
+            ld = wrD.LineCount;
+
+            for (int i = 0; i < la; i++) {
+                a += wrA.GetLineText(i);
+            }
+            for (int i = 0; i < lb; i++) {
+                b += wrB.GetLineText(i);
+            }
+            for (int i = 0; i < lc; i++) {
+                c += wrC.GetLineText(i);
+            }
+            for (int i = 0; i < ld; i++) {
+                d += wrD.GetLineText(i);
+            }
+
+            mw.Update(a, b, c, d,
+                wrSizeA.GetLineText(0), wrSizeB.GetLineText(0), wrSizeC.GetLineText(0), wrSizeD.GetLineText(0));
+        }
+
+        private void Window_Closed(object sender, EventArgs e) {
+            mw.Close();
+        }
+
+        private void aP_Click(object sender, RoutedEventArgs e) {
+            mw.UpdDim("a+");
+            wrSizeA.Text = (int.Parse(wrSizeA.GetLineText(0))+1).ToString();
+        }
+        private void aM_Click(object sender, RoutedEventArgs e) {
+            mw.UpdDim("a-");
+            wrSizeA.Text = (int.Parse(wrSizeA.GetLineText(0)) - 1).ToString();
+        }
+
+        private void bP_Click(object sender, RoutedEventArgs e) {
+            mw.UpdDim("b+");
+            wrSizeB.Text = (int.Parse(wrSizeB.GetLineText(0)) + 1).ToString();
+        }
+        private void bM_Click(object sender, RoutedEventArgs e) {
+            mw.UpdDim("b-");
+            wrSizeB.Text = (int.Parse(wrSizeB.GetLineText(0)) - 1).ToString();
+        }
+
+        private void cP_Click(object sender, RoutedEventArgs e) {
+            mw.UpdDim("c+");
+            wrSizeC.Text = (int.Parse(wrSizeC.GetLineText(0)) + 1).ToString();
+        }
+        private void cM_Click(object sender, RoutedEventArgs e) {
+            mw.UpdDim("c-");
+            wrSizeC.Text = (int.Parse(wrSizeC.GetLineText(0)) - 1).ToString();
+        }
+
+        private void dP_Click(object sender, RoutedEventArgs e) {
+            mw.UpdDim("d+");
+            wrSizeD.Text = (int.Parse(wrSizeD.GetLineText(0)) + 1).ToString();
+        }
+        private void dM_Click(object sender, RoutedEventArgs e) {
+            mw.UpdDim("d-");
+            wrSizeD.Text = (int.Parse(wrSizeD.GetLineText(0)) - 1).ToString();
+        }
+    }
+}
